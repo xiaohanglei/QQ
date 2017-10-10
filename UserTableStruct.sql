6e1e93d3744973b4321bb66798ebb727fd0ef83a
Xiaohang Lei <xiaohangl@sina.cn>
2017-10-10 15:13:59 +0800

    添加User的数据表结构（sql语句）

---
+++ UserTableStruct.sql
@@ -0,0 +1,36 @@
+/*
+SQLyog  v12.2.6 (64 bit)
+MySQL - 5.6.21-log : Database - dddd
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`dddd` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `dddd`;
+
+/*Table structure for table `heihei` */
+
+DROP TABLE IF EXISTS `heihei`;
+
+CREATE TABLE `heihei` (
+  `user` varchar(20) NOT NULL,
+  `password` varchar(20) DEFAULT NULL,
+  `nicheng` varchar(20) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `state` tinyint(1) DEFAULT NULL,
+  `ip` varchar(40) DEFAULT NULL,
+  PRIMARY KEY (`user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
